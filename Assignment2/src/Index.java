5112508ebc1356a7604633ccacf0c4748a8b8642
siddh <siddh@Siddhi>
2022-08-23 21:46:38 +0530

    This is project4

---
+++ Assignment2/src/Index.java
@@ -0,0 +1,15 @@
+
+public class Index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[]= {75,4,56,5,36,100,77,18,29,94};//initialization and declaration of array.
+	    for(int i=0;i<array.length;i++) {
+	    	if(array[i]==100) //condition check 100 is available at which position.
+	    	{
+	    		System.out.print("Index of 100 is "+i);//print the index
+	    	}
+	    }
+	}
+
+}
